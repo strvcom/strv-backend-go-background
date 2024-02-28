767cfae21ac16d5d369d27706c5ef88003694656
Robert Rossmann <rr.rossmann@me.com>
2024-02-28 16:54:52 +0100

    feat: remove generic parameter 🥺
    Having the Meta field generic was nice but Go cannot yet infer the generic type on many places yet and it was just very cumbersome to work with while it brought minimal benefits.
    
    It's sad to see it go away but I belive it's for the best.

--- background.go
+++ background.go
@@ -18,22 +18,22 @@ import (
 // before they can run to completion. This package is not a replacement for a proper task queue system but it is a great
 // package to schedule the queue jobs without the customer waiting for that to happen while at the same time being able
 // to wait for all those goroutines to finish before allowing the process to exit.
-type Manager[Meta any] struct {
+type Manager struct {
 	wg               sync.WaitGroup
 	len              int
 	stalledThreshold time.Duration
-	hooks            Hooks[Meta]
+	hooks            Hooks
 	strategies       Retry
 }
 
 // Options provides a means for configuring the background manager and attaching hooks to it.
-type Options[Meta any] struct {
+type Options struct {
 	// StalledThreshold is the amount of time within which the goroutine should return before it is considered stalled.
 	// Note that no effort is made to actually kill the goroutine.
 	StalledThreshold time.Duration
 	// Hooks allow you to register monitoring functions that are called when something happens with the goroutines that
 	// you schedule. These are useful for logging, monitoring, etc.
-	Hooks Hooks[Meta]
+	Hooks Hooks
 	// DefaultRetry defines the default retry strategies that will be used for all tasks unless overridden by the task.
 	// Several strategies are provided by github.com/kamilsk/retry/v5/strategy package.
 	DefaultRetry Retry
@@ -41,24 +41,24 @@ type Options[Meta any] struct {
 
 // Hooks are a set of functions that are called when certain events happen with the goroutines that you schedule. All of
 // them are optional; implement only those you need.
-type Hooks[Meta any] struct {
+type Hooks struct {
 	// OnTaskAdded is called immediately after calling Run().
-	OnTaskAdded func(ctx context.Context, meta Meta)
+	OnTaskAdded func(ctx context.Context, meta Metadata)
 	// OnTaskSucceeded is called immediately after Task returns with no error.
-	OnTaskSucceeded func(ctx context.Context, meta Meta)
+	OnTaskSucceeded func(ctx context.Context, meta Metadata)
 	// OnTaskFailed is called immediately after Task returns with an error.
-	OnTaskFailed func(ctx context.Context, meta Meta, err error)
+	OnTaskFailed func(ctx context.Context, meta Metadata, err error)
 	// OnGoroutineStalled is called when the goroutine does not return within the StalledThreshold. You can use this to
 	// make sure your goroutines do not take excessive amounts of time to run to completion.
-	OnGoroutineStalled func(ctx context.Context, meta Meta)
+	OnGoroutineStalled func(ctx context.Context, meta Metadata)
 }
 
-// TaskDefinition describes how a unit of work (a Task) should be executed.
-type TaskDefinition[Meta any] struct {
-	// Task is the function to be executed in a goroutine.
-	Task Task
-	// Meta is whatever you wish to associate with the task.
-	Meta Meta
+// Task describes how a unit of work (a function) should be executed.
+type Task struct {
+	// Fn is the function to be executed in a goroutine.
+	Fn Fn
+	// Meta is whatever custom information you wish to associate with the task.
+	Meta Metadata
 	// Retry defines how the task should be retried in case of failure (if at all). This overrides the default retry
 	// strategies you might have configured in the Manager. Several strategies are provided by
 	// github.com/kamilsk/retry/v5/strategy package.
@@ -69,20 +69,21 @@ type TaskDefinition[Meta any] struct {
 // github.com/kamilsk/retry/v5/strategy package.
 type Retry []strategy.Strategy
 
-// Task is the function to be executed in a goroutine
-type Task func(ctx context.Context) error
+// Fn is the function to be executed in a goroutine
+type Fn func(ctx context.Context) error
 
-// NilMeta is a type that can be used as Meta generic type when you do not need to associate any metadata with the task.
-type NilMeta *struct{}
+// Metadata is whatever custom information you wish to associate with a task. This information will be available in your
+// lifecycle hooks to help you identify which task is being processed.
+type Metadata map[string]string
 
 // NewManager creates a new instance of Manager with default options and no hooks.
-func NewManager() *Manager[NilMeta] {
-	return &Manager[NilMeta]{}
+func NewManager() *Manager {
+	return &Manager{}
 }
 
 // NewManagerWithOptions creates a new instance of Manager with the provided options and hooks.
-func NewManagerWithOptions[Meta any](options Options[Meta]) *Manager[Meta] {
-	return &Manager[Meta]{
+func NewManagerWithOptions(options Options) *Manager {
+	return &Manager{
 		stalledThreshold: options.StalledThreshold,
 		hooks:            options.Hooks,
 		strategies:       options.DefaultRetry,
@@ -90,13 +91,13 @@ func NewManagerWithOptions[Meta any](options Options[Meta]) *Manager[Meta] {
 }
 
 // Run schedules the provided task to be executed in a goroutine.
-func (m *Manager[Meta]) Run(ctx context.Context, task Task) {
-	definition := TaskDefinition[Meta]{Task: task}
+func (m *Manager) Run(ctx context.Context, task Fn) {
+	definition := Task{Fn: task}
 	m.RunTaskDefinition(ctx, definition)
 }
 
 // RunTaskDefinition schedules the provided task definition to be executed in a goroutine.
-func (m *Manager[Meta]) RunTaskDefinition(ctx context.Context, definition TaskDefinition[Meta]) {
+func (m *Manager) RunTaskDefinition(ctx context.Context, definition Task) {
 	m.callOnTaskAdded(ctx, definition.Meta)
 	m.wg.Add(1)
 	m.len++
@@ -109,18 +110,18 @@ func (m *Manager[Meta]) RunTaskDefinition(ctx context.Context, definition TaskDe
 }
 
 // Wait blocks until all scheduled tasks have finished.
-func (m *Manager[Meta]) Wait() {
+func (m *Manager) Wait() {
 	m.wg.Wait()
 }
 
 // Len returns the number of currently running tasks.
-func (m *Manager[Meta]) Len() int {
+func (m *Manager) Len() int {
 	return m.len
 }
 
-func (m *Manager[Meta]) run(ctx context.Context, definition TaskDefinition[Meta], done chan<- bool) {
+func (m *Manager) run(ctx context.Context, definition Task, done chan<- bool) {
 	strategies := mkstrategies(m.strategies, definition.Retry)
-	err := retry.Do(ctx, definition.Task, strategies...)
+	err := retry.Do(ctx, definition.Fn, strategies...)
 	done <- true
 	m.wg.Done()
 	m.len--
@@ -132,7 +133,7 @@ func (m *Manager[Meta]) run(ctx context.Context, definition TaskDefinition[Meta]
 	}
 }
 
-func (m *Manager[Meta]) ticktock(ctx context.Context, meta Meta, done <-chan bool) {
+func (m *Manager) ticktock(ctx context.Context, meta Metadata, done <-chan bool) {
 	timeout := mktimeout(m.stalledThreshold)
 	select {
 	case <-done:
@@ -143,25 +144,25 @@ func (m *Manager[Meta]) ticktock(ctx context.Context, meta Meta, done <-chan boo
 	}
 }
 
-func (m *Manager[Meta]) callOnTaskFailed(ctx context.Context, meta Meta, err error) {
+func (m *Manager) callOnTaskFailed(ctx context.Context, meta Metadata, err error) {
 	if m.hooks.OnTaskFailed != nil {
 		m.hooks.OnTaskFailed(ctx, meta, err)
 	}
 }
 
-func (m *Manager[Meta]) callOnTaskSucceeded(ctx context.Context, meta Meta) {
+func (m *Manager) callOnTaskSucceeded(ctx context.Context, meta Metadata) {
 	if m.hooks.OnTaskSucceeded != nil {
 		m.hooks.OnTaskSucceeded(ctx, meta)
 	}
 }
 
-func (m *Manager[Meta]) callOnTaskAdded(ctx context.Context, meta Meta) {
+func (m *Manager) callOnTaskAdded(ctx context.Context, meta Metadata) {
 	if m.hooks.OnTaskAdded != nil {
 		m.hooks.OnTaskAdded(ctx, meta)
 	}
 }
 
-func (m *Manager[Meta]) callOnGoroutineStalled(ctx context.Context, meta Meta) {
+func (m *Manager) callOnGoroutineStalled(ctx context.Context, meta Metadata) {
 	if m.hooks.OnGoroutineStalled != nil {
 		m.hooks.OnGoroutineStalled(ctx, meta)
 	}
